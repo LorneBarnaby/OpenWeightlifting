d679da26677baeda5cd9854e88e15f228b1dc1ba
Lorne Barnaby <lorne123barnaby@gmail.com>
2024-11-16 00:14:02 +0000

    Use aysnclist to table cols as in next docs

--- frontend/components/molecules/eventTable.tsx
+++ frontend/components/molecules/eventTable.tsx
@@ -7,29 +7,58 @@ import {
   TableBody, Link,
 } from '@nextui-org/react'
 import { LifterResult } from '@/api/fetchLifterData/fetchLifterDataTypes'
+import {AsyncListData, useAsyncList} from "@react-stately/data";
+import { SortDescriptor} from "@react-types/shared";
 
 export const EventTable = ({
                                history,
                              }: {
   history: LifterResult[]
 }) => {
+
+  let list = useAsyncList({
+    async load () {
+      return {items: history}
+    },
+    async sort({items, sortDescriptor}:{items:LifterResult[], sortDescriptor:SortDescriptor}) {
+      return {
+        items: items.sort((a, b) => {
+          let col = (sortDescriptor.column?? "sinclair") as keyof LifterResult;  
+          let first = parseInt(a[col].toString());
+          let second = parseInt(b[col].toString());
+
+          let cmp = first < second ? -1 : 1;
+
+          // Rank misses at bigger weights higher
+          if(first < 0 && second < 0){
+            cmp *= -1
+          }
+          if (sortDescriptor.direction === "descending") {
+            cmp *= -1;
+          }
+          return cmp;
+        }),
+      };
+    },
+  });
+
   return (
-    <Table>
+    <Table onSortChange={list.sort} sortDescriptor={list.sortDescriptor}>
       <TableHeader>
         <TableColumn>Date</TableColumn>
         <TableColumn>Name</TableColumn>
-        <TableColumn>Bodyweight</TableColumn>
-        <TableColumn>1st Snatch</TableColumn>
-        <TableColumn>2nd Snatch</TableColumn>
-        <TableColumn>3rd Snatch</TableColumn>
-        <TableColumn>1st C&J</TableColumn>
-        <TableColumn>2nd C&J</TableColumn>
-        <TableColumn>3rd C&J</TableColumn>
-        <TableColumn>Total</TableColumn>
-        <TableColumn>Sinclair</TableColumn>
+        <TableColumn allowsSorting key="bodyweight">Bodyweight</TableColumn>
+        <TableColumn allowsSorting key="snatch_1">1st Snatch</TableColumn>
+        <TableColumn allowsSorting key="snatch_2">2nd Snatch</TableColumn>
+        <TableColumn allowsSorting key="snatch_3">3rd Snatch</TableColumn>
+        <TableColumn allowsSorting key="cj_1">1st C&J</TableColumn>
+        <TableColumn allowsSorting key="cj_2">2nd C&J</TableColumn>
+        <TableColumn allowsSorting key="cj_3">3rd C&J</TableColumn>
+        <TableColumn allowsSorting key="total">Total</TableColumn>
+        <TableColumn allowsSorting key="sinclair">Sinclair</TableColumn>
       </TableHeader>
       <TableBody>
-        {history.map((lift, index) => {
+        {list.items.map((lift, index) => {
           const {
             date,
             lifter_name,
